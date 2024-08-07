1a83b19b60f4e4840adcf6bfa934523d9d07ae0d
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-08-07 13:49:03 -0400

    initial commit

---
+++ src/Pounds.java
@@ -0,0 +1,8 @@
+public class Pounds {
+        public static void main(String[] args) {
+            double pounds = 200;
+            double kilos = pounds * 0.45359237;
+            System.out.println("Kilograms" + kilos);
+        }
+    }
+
